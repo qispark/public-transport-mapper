4944e2092b69882b56b0bb93c230ba8284ece365
Robin Tang <robin@qsstudio.co>
2025-04-23 09:28:35 +1000

    use example based on https://github.com/Esri/jsapi-resources/tree/main/component-samples/map-components/samples/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Import ArcGIS map components
+import '@arcgis/map-components/components/arcgis-map'
+import '@arcgis/map-components/components/arcgis-legend'
+import '@arcgis/map-components/components/arcgis-search'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <arcgis-map
+      item-id="b9745d4721fc46c0901f104b828f6f2c"
+      onarcgisViewReadyChange={event => {
+        console.log('MapView ready', event)
+      }}
+    >
+      <arcgis-search position="top-right"></arcgis-search>
+      <arcgis-legend position="bottom-left"></arcgis-legend>
+    </arcgis-map>
   )
 }
-
-export default App
